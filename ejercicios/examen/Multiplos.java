fedbeec502ac59d0b6847ca3aa324e45104d92e6
Miriam <dgmacram@hotmail.com>
2016-03-08 20:37:16 -0600

    Examen

---
+++ ejercicios/examen/Multiplos.java
@@ -0,0 +1,25 @@
+import java.io.*;
+public class Multiplos {
+	public satatic void main (String [] args){
+		int a, b, aux, suma;
+
+		System.out.println ("Dame los numeros de la suma");
+		a = sc.next.in (System.in);
+		b = sc.mext.in (System.in);
+
+		if (a || b <= o){
+			System.out.println ("Haz ingresado un numero incorrecto, ingresa de nuevo")
+		}
+		if (a && b % 5 == 0){
+			suma = a + b;
+		System. out. println ("El resultado de la sumaes")+suma;
+}
+		if (a>b) {
+			a =] aux
+		b = a
+		
+		System out. println ("El valor a es:  ") +b("El valor de b es:") +aux;
+			
+		}
+	}
+}
